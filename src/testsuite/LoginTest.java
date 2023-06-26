ea14f32460965a1a2d4dc34f10cb2f5e63d0b3c5
Vicky122511 <vicky.shah7928@yahoo.com>
2023-06-26 23:13:01 +0100

    Nevil

---
+++ src/testsuite/LoginTest.java
@@ -0,0 +1,71 @@
+package testsuite;
+
+import browsefactory.BaseTest;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class LoginTest extends BaseTest
+{
+    String baseurl = "https://www.saucedemo.com/";
+    String expectedText,actualText,count1;
+
+    @Before
+    public void setUp()
+    {
+        openBrowser(baseurl);
+    }
+
+    @Test
+    public void userShouldLoginSuccessfullyWithValidCredentials()
+    {
+        //Enter “standard_user” username
+        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+
+        //Enter “secret_sauce” password
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+
+        //Click on ‘LOGIN’ button
+        driver.findElement(By.xpath("//input[@id='login-button']")).click();
+
+        //Verify the text “PRODUCTS”
+        expectedText="Products";
+        actualText=driver.findElement(By.xpath("//span[text()='Products']")).getText();
+        Assert.assertEquals("Both Text Are Not Equal",expectedText,actualText);
+
+    }
+
+    @Test
+    public void verifyThatSixProductsAreDisplayedOnPage()
+    {
+        //Enter “standard_user” username
+        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+
+        //Enter “secret_sauce” password
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+
+        //Click on ‘LOGIN’ button
+        driver.findElement(By.xpath("//input[@id='login-button']")).click();
+
+        //Verify that six products are displayed on page
+        // Verify 6 Products are displayed on page
+        List<WebElement> productList = driver.findElements(By.xpath("//div[@class = 'inventory_item']"));
+        System.out.println("Total products are : " + productList.size());
+        // Verify the number of products are 6
+        int expectedSize = 6;
+        int actualSize = productList.size();
+        Assert.assertEquals("Products size is not match ", expectedSize, actualSize);
+
+    }
+
+    @After
+    public void tearDown()
+    {
+        closeBrowser();
+    }
+}
